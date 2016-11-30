c467e21d82902c03aeeb3c6eb91412b6af4ab29a
Vlad Kolesnikov <vlad.kolesn@hotmail.com>
2016-11-30 17:50:45 +0000

    added base logic to controller and views

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,2 +1,48 @@
 class DrinksController < ApplicationController
+
+    def index
+        @drinks = Drink.all
+    end
+    
+    def show
+        @drink = Drink.find(params[:id])
+    end
+
+    def new #display form
+        @drink = Drink.new
+    end
+    
+    def create #save new record
+        @drink = Drink.new(allowed_params)
+        if @drink.save
+            redirect_to drinks_path #index
+        else
+            redender 'new'
+        end 
+    end
+
+    def edit
+        @drink = Drink.find(params[:id])
+    end
+    
+    def update #save changes
+        @drink = Drink.find(params[:id])
+        if @drink.update_attributes(allowed_params)
+            redirect_to drinks_path #index
+        #else
+            # redender 'edit' #new
+        end 
+    end
+    
+    def destroy
+        @drink = Drink.find(params[:id])
+        @drink.destroy
+        redirect_to drinks_path
+    end
+    
+    private
+        def allowed_params
+            params.require(:drink).permit(:name, :size)
+        end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'drinks#index'
+  resources :drinks
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
